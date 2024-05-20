c64a82e1ab9758ad7e5426b427f9f7d5653b3c44
elhussary <ahmedwaeljob@gmail.com>
2024-05-20 19:44:23 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,2 @@
+console.log("Hello")
+import "../css/style.css"
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./src/**/*.{js,ts,jsx,tsx,html}"],
+    theme: {
+        extend: {},
+        colors: {
+            primary: "#bc44dd",
+            secondray: "#7e32dc",
+        },
+        container: {
+            center: true,
+            padding: "2rem",
+            screens: {
+                "2xl": "1200px",
+            },
+        },
+    },
+    plugins: [],
+};
